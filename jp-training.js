5f53a8381197d3fb33f0c37ad80668ba869fb3f7
Deadfaced <btteixeira.bt@gmail.com>
2023-03-02 23:07:13 +0000

    finished social links, changed buttons' color, removed footer

---
+++ jp-training.js
@@ -0,0 +1,35 @@
+let navbar = document.getElementById("navbar");
+let logo = document.getElementById("logo");
+let logo2 = document.getElementById("logo2");
+let img = document.getElementById("img");
+let img2 = document.getElementById("img2");
+
+logo.addEventListener("mouseover", () => {
+    navbar.style.width = "75%";
+    img.style.width = "75px";
+    img.style.height = "75px";
+    img2.style.height = "75px";
+    img2.style.width = "75px";
+})
+logo.addEventListener("mouseout", () => {
+    navbar.style.width = "50%"; 
+    img.style.width = "50px"; 
+    img.style.height = "50px";
+    img2.style.height = "50px";
+    img2.style.width = "50px";
+})
+
+logo2.addEventListener("mouseover", () => {
+    navbar.style.width = "75%";
+    img.style.width = "75px";
+    img.style.height = "75px";
+    img2.style.height = "75px";
+    img2.style.width = "75px";
+})
+logo2.addEventListener("mouseout", () => {
+    navbar.style.width = "50%"; 
+    img.style.width = "50px"; 
+    img.style.height = "50px";
+    img2.style.height = "50px";
+    img2.style.width = "50px";
+})
